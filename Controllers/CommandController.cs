d822b160fc766aa2008a14ac6573e1ed81ae4ab3
fulloption <keang_ll@hotmail.com>
2023-02-27 15:45:27 +0700

    add controllers/commandcontroller

---
+++ Controllers/CommandController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMMANDER.Controllers
+{
+    //
+    [Route("api/controller")]
+    [ApiController]
+    public class CommandsController : ControllerBase    
+    {
+        
+    }
+}
