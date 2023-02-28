db6dd40e8407a65610a9db3099e459b809443eae
fulloption <keang_ll@hotmail.com>
2023-02-28 10:39:03 +0700

    Controller test

--- Controllers/CommandController.cs
+++ Controllers/CommandController.cs
@@ -1,3 +1,5 @@
+using COMMANDER.Data;
+using COMMANDER.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace COMMANDER.Controllers
@@ -7,6 +9,25 @@ namespace COMMANDER.Controllers
     [ApiController]
     public class CommandsController : ControllerBase    
     {
-        
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+
+        // get api/Command
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommander()
+        {
+            var commandItem = _repository.GetAppCommands();
+
+            return Ok(commandItem);
+        }
+
+
+        // get api/command/{id}
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+
+            return Ok(commandItem);
+        }
     }
 }
